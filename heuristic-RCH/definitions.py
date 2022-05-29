6b8a5792d9bc2f4c1690756c57c4f87ee7cb1f09
Yarin <dado.yarin@gmail.com>
2022-05-29 20:49:06 +0300

    working (i.e. no runtime exceptions)

--- heuristic-RCH/definitions.py
+++ heuristic-RCH/definitions.py
@@ -1,8 +1,41 @@
+import random
 from enum import Enum, unique, auto
 from tkinter.tix import DECREASING
 
 
+DEBUG_MODE = True
 DIMENSIONS = 3
+
+
+def print_debug(message):
+    if DEBUG_MODE:
+        print(message)
+
+
+class Point:
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+
+class Size:
+    def __init__(self, width: int, height: int, depth: int):
+        self.w = width
+        self.h = height
+        self.d = depth
+
+    def __eq__(self, other):
+        return self.w == other.w and self.h == other.h and self.d == other.d
+
+
+class Container:
+    def __init__(self, size: Size, weight_limit: int):
+        self.size = size
+        self.weight_limit = weight_limit
 
 
 @unique
@@ -42,6 +75,15 @@ class RotationType(Enum):
     XZ   = auto()
     Y    = auto()
     XY   = auto()
+
+
+# rotation in degrees
+class Rotation():
+    def __init__(self):
+        self.angle = 0
+
+    def add(self, rotation_angle):
+        self.angle = (self.angle + rotation_angle) % 360
 
 
 '''
@@ -85,7 +127,7 @@ class Box:
     def __init__(
         self, 
         box_type: str,
-        size: tuple[int, int, int], 
+        size: Size, 
         weight: int, # C1
         priority: int, # C3
         rotations: set[RotationType], # C4
@@ -101,13 +143,44 @@ class Box:
         self.stackable = stackable
         self.combination = combination
         self.customer_code = customer_code
-        self.volume = size[0] * size[1] * size[2]
-        # TODO: calculate taxability
+        self.volume = size.w * size.h * size.d
+        # TODO: calculate alpha
         alpha = weight / 1
         self.taxability = max(weight, alpha * self.volume)
 
+        self.rotation_x = Rotation()
+        self.rotation_y = Rotation()
+        self.rotation_z = Rotation()
+
     def get_common_dims(self, other):
-        return [i for i in range(DIMENSIONS) if self.size[i] == other.size[i]]
+        common_dims = []
+        if self.size.w == other.size.w: common_dims.append('w')
+        if self.size.h == other.size.h: common_dims.append('h')
+        if self.size.d == other.size.d: common_dims.append('d')
+        return common_dims
+
+    def rotate(self, rotation_type: RotationType = None) -> None:
+        if rotation_type is None:
+            rotation_type = random.choice(list(self.rotations))
+        
+        w, h, d = self.size.w, self.size.h, self.size.d
+        if rotation_type == RotationType.X:
+            self.size = Size(w, d, h)
+            self.rotation_x.add(90)
+        elif rotation_type == RotationType.Z:
+            self.size = Size(h, w, d)
+            self.rotation_z.add(90)
+        elif rotation_type == RotationType.XZ:
+            self.size = Size(h, d, w)
+            self.rotation_x.add(90)
+            self.rotation_z.add(90)
+        elif rotation_type == RotationType.Y:
+            self.size = Size(d, h, w)
+            self.rotation_y.add(90)
+        elif rotation_type == RotationType.XY:
+            self.size = Size(d, w, h)
+            self.rotation_x.add(90)
+            self.rotation_y.add(90)
 
     # create a new box by combining two other boxes
     @classmethod
@@ -120,13 +193,156 @@ class Box:
         stackable = box_a.stackable and box_b.stackable
         rotations = box_a.rotations.intersection(box_b.rotations)
 
+        # TODO: flip h and d?
         # size depends on the type of combination
         if combination.combination_type in [CombinationType.WH_LOWER, CombinationType.WH_HIGHER]:
-            size = (box_a.size[0], box_a.size[1], box_a.size[2] + box_b.size[2])
+            size = Size(box_a.size.w, box_a.size.h, box_a.size.d + box_b.size.d)
         elif combination.combination_type in [CombinationType.WD_LOWER, CombinationType.WD_HIGHER]:
-            size = (box_a.size[0], box_a.size[1] + box_b.size[1], box_a.size[2])
+            size = Size(box_a.size.w, box_a.size.h + box_b.size.h, box_a.size.d)
         else:
-            size = (box_a.size[0] + box_b.size[0], box_a.size[1], box_a.size[2])
+            size = Size(box_a.size.w + box_b.size.w, box_a.size.h, box_a.size.d)
 
         # call constructor
         return cls('combined', size, weight, priority, rotations, stackable, combination)
+
+
+class PositionedBox:
+    def __init__(self, box: Box, point: Point):
+        self.box = box
+        self.point = point
+
+
+class OccupiedSpace(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class UsedSpace:
+    def __init__(self, size: Size):
+        self.size = size
+        self.used_space_map = [[[False for _ in range(size.d)] for _ in range(size.h)] for _ in range(size.w)]
+
+    def add(self, box: Box, point: Point) -> None:
+        # TODO: flip h and d?
+        for x in range(point.x, point.x + box.size.w):
+            for y in range(point.y, point.y + box.size.h):
+                for z in range(point.z, point.z + box.size.d):
+                    if not self.used_space_map[x][y][z]:
+                        self.used_space_map[x][y][z] = True
+                    else:
+                        raise OccupiedSpace(f'occupied:: x={x} y={y} z={z} type={box.box_type}')
+
+    def can_be_added(self, box: Box, point: Point) -> bool:
+        # TODO: flip h and d?
+        for x in range(point.x, point.x + box.size.w):
+            for y in range(point.y, point.y + box.size.h):
+                for z in range(point.z, point.z + box.size.d):
+                    if self.used_space_map[x][y][z]: 
+                        return False
+        return True
+
+
+class Packing:
+    def __init__(self, container: Container):
+        self.container = container
+        self.positioned_boxes = []
+        self.total_weight = 0
+        self.used_space = UsedSpace(container.size)
+
+    def add(self, box: Box, point: Point) -> None:
+        positioned_box = PositionedBox(box, point)
+        self.positioned_boxes.append(positioned_box)
+        self.total_weight += box.weight
+        # TODO: flip h and d?
+        self.used_space.add(box, point)
+
+    def can_be_added(self, box: Box, point: Point) -> bool:
+        # does the box exceed the container weight limit
+        new_total_weight = box.weight + self.total_weight
+        if new_total_weight > self.container.weight_limit:
+            return False
+        
+        # does the box exceed the container boundaries
+        # TODO: flip h and d?
+        corner = Point(point.x + box.size.w, point.y + box.size.h, point.z + box.size.d)
+        if corner.x >= self.container.size.w or corner.y >= self.container.size.h or corner.z >= self.container.size.d:
+            return False
+        
+        # does the box overlap another box
+        if not self.used_space.can_be_added(box, point):
+            return False
+        
+        # TODO: is the box above nonstackable box
+        # IDEA: upon adding nonstackable box -> mark all space above as occupied
+        
+        return True
+
+
+class PackingInput:
+    def __init__(self, json_data):
+        # init container
+        container = json_data['container']
+        container_size = Size(container['width'], container['height'], container['depth'])
+        self.container = Container(container_size, container['maxWeight'])
+        
+        # init boxes
+        self.boxes = []
+        packages = json_data['packages']
+        for pkg in packages:
+            for _ in range(pkg['amount']):
+                box = Box(
+                    box_type=pkg['type'],
+                    size=Size(pkg['width'], pkg['height'], pkg['depth']),
+                    weight=pkg['weight'],
+                    priority=pkg['priority'],
+                    rotations=set(list(RotationType)) if pkg['canRotate'] else set([RotationType.NONE]),
+                    stackable=pkg['canStackAbove'],
+                    combination=None,
+                    customer_code=1
+                )
+                self.boxes.append(box)
+
+
+class PackingResult:
+    def __init__(self, error: str = None, packing: Packing = None):
+        self.error = error
+        self.packing = packing
+
+    def to_json(self):
+        json_data = {}
+        if self.error is not None:
+            json_data['error'] = self.error
+        if self.packing is not None:
+            container_size = self.packing.container.size
+            json_data['container'] = {
+                "width": container_size.w,
+                "height": container_size.h,
+                "depth": container_size.d
+            }
+            positioned_boxes = self.packing.positioned_boxes
+            package_types = {}
+            json_data['solution'] = []
+            for b in positioned_boxes:
+                box_type = b.box.box_type
+                if box_type not in package_types:
+                    package_types[box_type] = b.box
+                json_data['solution'].append({
+                    "type": box_type,
+                    "x": b.point.x,
+                    "y": b.point.y,
+                    "z": b.point.z,
+                    "rotation-x": b.box.rotation_x.angle,
+                    "rotation-y": b.box.rotation_y.angle, 
+                    "rotation-z": b.box.rotation_z.angle
+                })
+            json_data['packages'] = []
+            for key in package_types.keys():
+                box = package_types[key]
+                json_data['packages'].append({
+                    "type": key,
+                    "width": box.size.w,
+                    "height": box.size.h,
+                    "depth": box.size.d
+                })
+            
+        return json_data
---
+++ heuristic-RCH/rch.py
@@ -0,0 +1,226 @@
+# the code is based on article: "An optimization approach for a complex real-life container loading problem"
+# https://www.researchgate.net/publication/355421342_An_optimization_approach_for_a_complex_real-life_container_loading_problem
+
+
+import traceback
+import random
+import json
+
+from matplotlib import container
+from definitions import *
+
+
+# TODO: Packing list order matters? i.e. order of packing (Algorithm 2 output)
+
+
+SKIP_COMBINE_PROBABILITY = 0.5
+# TODO: (4.5): "N is potentially a large number to the order of hundreds of thousands"
+ALGORITHM_REPEAT_COUNT = 1
+REORDER_PROBABILITY = 0.5
+REORDER_RATIO_OFFSET = 0.3
+REORDER_RATIO_LOWER_BOUND = 1 - REORDER_RATIO_OFFSET
+REORDER_RATIO_HIGHER_BOUND = 1 + REORDER_RATIO_OFFSET
+
+
+def chance(probability: float) -> bool:
+    return random.random() < probability
+
+
+def combine(a: Box, b: Box, common_dims: list[int]) -> list[Box]:
+    amount_of_common_dims = len(common_dims)
+    if amount_of_common_dims == 3:
+        combination_type = random.choice(list(CombinationType))
+    elif amount_of_common_dims == 2:
+        if 'w' in common_dims and 'h' in common_dims:
+            combination_type = random.choice([CombinationType.WH_LOWER, CombinationType.WH_HIGHER])
+        elif 'w' in common_dims and 'd' in common_dims:
+            combination_type = random.choice([CombinationType.WD_LOWER, CombinationType.WD_HIGHER])
+        else:
+            combination_type = random.choice([CombinationType.HD_LOWER, CombinationType.HD_HIGHER])
+
+    combination = Combination(a, b, combination_type)
+    return Box.from_boxes(combination)
+
+
+def preprocess_boxes(boxes: list[Box]) -> list[Box]:
+    processed_boxes = []
+    combined_boxes = []
+    iterable = reversed(range(len(boxes)))
+    for i in iterable:
+        for j in iterable:
+            if i == j:
+                continue
+            a = boxes[i]
+            b = boxes[j]
+            common_dims = a.get_common_dims(b)
+            if len(common_dims) >= 2:
+                # keep track of boxes that were combined (they'll be removed from the return value)
+                combined_boxes.append(a)
+                combined_boxes.append(b)
+                # add the combination to processed_boxes
+                processed_boxes.append(combine(a, b, common_dims))
+    # add all boxes that were not combined to the return value
+    processed_boxes.extend([box for box in boxes if box not in combined_boxes])
+    return processed_boxes
+
+
+def sort_boxes(boxes: list[Box]) -> None:
+    sorting_type = random.choice(list(SortingType))
+    if sorting_type == SortingType.DECREASING_TAXABILITY:
+        boxes.sort(key=lambda box: box.taxability, reverse=True)
+    elif sorting_type == SortingType.DECREASING_PRIORITY:
+        boxes.sort(key=lambda box: (box.priority, box.taxability), reverse=True)
+    else:
+        boxes.sort(key=lambda box: (box.customer_code, box.taxability), reverse=True)
+
+
+def perturb_phase1(boxes: list[Box]) -> None:
+    # TODO: update possible rotations?
+    perturb_rotation = random.choice(list(PerturbRotation))
+    if perturb_rotation == PerturbRotation.INDIVIDUAL:
+        for box in boxes:
+            box.rotate()
+    else: # IDENTICAL
+        rotation_per_type = {}
+        for box in boxes:
+            if box.box_type not in rotation_per_type:
+                rotation_per_type[box.box_type] = random.choice(list(box.rotations))
+            box.rotate(rotation_per_type[box.box_type])
+
+
+def perturb_phase2(boxes: list[Box]) -> None:
+    perturb_rotation = random.choice(list(PerturbOrder))
+    for i in range(len(boxes) - 1):
+        key_curr = boxes[i].volume if perturb_rotation == PerturbOrder.VOLUME else boxes[i].weight
+        key_next = boxes[i + 1].volume if perturb_rotation == PerturbOrder.VOLUME else boxes[i + 1].weight
+        ratio = key_curr / key_next
+        if REORDER_RATIO_LOWER_BOUND <= ratio <= REORDER_RATIO_HIGHER_BOUND and chance(REORDER_PROBABILITY):
+            boxes[i], boxes[i + 1] = boxes[i + 1], boxes[i]
+
+
+# returns True iff a is "better" than b
+def is_better_fit_point(
+    a: Point,
+    b: Point,
+    box: Box
+) -> bool:
+    # TODO: better fit point
+    #    i.e. best_point is None OR larger proportion of the contact surface with the underlying item is used
+    #    FALL BACK to x value comparison (in case of tie)
+    if b is None:
+        return True
+    return True
+
+
+def find_best_point(box: Box, potential_points: list[Point], packing: Packing) -> Point:
+    best_point = None
+    for point in potential_points:
+        if packing.can_be_added(box, point) and is_better_fit_point(point, best_point, box):
+            best_point = point
+    return best_point
+
+
+def update_potential_points(box: Box, point: Point, potential_points: list[Point]) -> None:
+    potential_points.remove(point)
+    # add new potential points 
+    # TODO: flip h and d?
+    potential_points.append(Point(point.x + box.size.w, point.y, point.z))
+    potential_points.append(Point(point.x, point.y + box.size.h, point.z))
+    potential_points.append(Point(point.x, point.y, point.z + box.size.d))
+
+
+# Algorithm 2
+# Constructive Packing Phase of RCH
+def construct_packing(boxes: list[Box], container: Container) -> Packing:
+    potential_points = [Point(0, 0, 0), Point(container.size.w, 0, 0)] # P = {BLF, BRF}
+    retry_list = []
+    packing = Packing(container)
+    
+    for box in boxes: # foreach item i in L
+        best_point = find_best_point(box, potential_points, packing)
+        if best_point is not None:
+            packing.add(box, best_point)
+            update_potential_points(box, best_point, potential_points)
+        else:
+            # The insertion of box has failed
+            retry_list.append(box)
+    
+    print_debug(f'construct_packing:: boxes={len(boxes)} retry_list={len(retry_list)}')
+   
+    for box in retry_list:
+        box.rotate()
+        best_point = find_best_point(box, potential_points, packing)
+        if best_point is not None:
+            packing.add(box, best_point)
+            update_potential_points(box, best_point, potential_points)
+    
+    return packing
+
+
+# Algorithm 1
+# Randomized Constructive Heuristic
+def rch(packing_input: PackingInput) -> PackingResult:
+    container = packing_input.container
+    boxes = packing_input.boxes
+    packing_options = []
+    for _ in range(ALGORITHM_REPEAT_COUNT): # iteration n=1 to N
+        # pre-process phase (section 4.2)
+        if not chance(SKIP_COMBINE_PROBABILITY):
+            boxes = preprocess_boxes(boxes)
+
+        # sort/perturb phase (section 4.3)
+        sort_boxes(boxes)
+        perturb_phase1(boxes)
+        perturb_phase2(boxes)
+
+        # construct a solution (section 4.4)
+        packing = construct_packing(boxes, container)
+        packing_options.append(packing)
+
+        # TODO: check if packing is infeasible
+
+    # TODO: return all packings
+    result = PackingResult(packing=packing_options[0])
+    return result
+    
+
+def parse_input(input_data: bytearray):
+    parsed_data = json.loads(input_data)
+    return parsed_data
+
+
+def pack(input_data):
+    result = {}
+    try:
+        packing_input = PackingInput(input_data)
+        result = rch(packing_input)
+    except Exception as e:
+        result = PackingResult(error=f'Exception: {e}')
+        # TODO: remove
+        raise e
+    return result.to_json()
+
+
+def write_result_to_file(result):
+    with open('./heuristic-rch/output.json', 'w') as out_file:
+        out_file.write(result)
+
+
+def main():
+    input_path = './heuristic-rch/input.json'
+    try:
+        with open(input_path, 'r') as in_file:
+            input_data = in_file.read()
+            parsed_input = parse_input(input_data)
+            result = pack(parsed_input)
+            print('finished!')
+            result_json = result.to_json()
+            result_string = json.dumps(result_json, indent=2, sort_keys=False)
+            write_result_to_file(result_string)
+    except Exception as e:
+        print('received an exception', e)
+        traceback.print_exc()
+
+
+if __name__ == '__main__':
+    main()
